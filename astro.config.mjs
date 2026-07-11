63cc21774446caa71d04d169fb504a8e48499184
MOHSIN <mohsinuddin.handsome@gmail.com>
2026-07-12 00:17:01 +0530

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import Analytics from '@vercel/analytics/astro';
 
 export default defineConfig({
   site: 'https://medgptai.droploop.in',
   output: 'static',
-  integrations: [sitemap(), mdx(), Analytics()],
+  integrations: [sitemap(), mdx()],
   image: {
     domains: [],
   },
